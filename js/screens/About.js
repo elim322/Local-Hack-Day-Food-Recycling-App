53373ba8a0366ea6d556c04a90e9965061087489
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-12-01 13:05:12 -0800

    initial work about page

--- js/screens/About.js
+++ js/screens/About.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { Text, View, Image, ScrollView } from "react-native";
 
-const About = () => {
-  return (
-    <View>
-      <Text>this is the about page</Text>
-    </View>
-  );
-};
+class About extends React.Component {
+  static navigationOptions = {
+    title: "About",
+    headerTitleStyle: {
+      color: "black"
+    }
+  };
+  render() {
+    return (
+      <ScrollView>
+        <Text>Food Buddy is a bundle of joy! </Text>
+        <Text>
+          We help reduce hunger in our city by connecting restaurants and
+          supermarkets to charity instituions. The idea is for them to donate
+          leftover food that they can no longer sell, instead of throwing away
+          loads of perfectly consumable food.
+        </Text>
+        <Text>In additon, this gesture also reducees the amount of trash.</Text>
+      </ScrollView>
+    );
+  }
+}
 
 export default About;
