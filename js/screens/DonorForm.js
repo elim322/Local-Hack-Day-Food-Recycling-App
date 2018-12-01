785cfe76a9afa380bf56649291b49d2b2572dffe
Jenny rose Gutierrez <Phichik@dhcp-206-12-39-114.ubcvisitor.wireless.ubc.ca>
2018-12-01 13:15:59 -0800

    working on the donor's form

--- js/screens/DonorForm.js
+++ js/screens/DonorForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import { withNavigation } from "react-navigation";
+import { Form, Field, FormSpy } from "react-final-form";
+import { Text, View, Image, ScrollView, Button } from "native-base";
 
 class DonorForm extends Component {
-  render() {}
+  render() {
+    <View>
+      <Form />
+    </View>;
+  }
 }
 
 export default withNavigation(DonorForm);
