22044db8197399a2c797b0f2add772a19386921d
Jenny rose Gutierrez <Phichik@dhcp-206-12-39-114.ubcvisitor.wireless.ubc.ca>
2018-12-01 13:02:26 -0800

    working on donorForm

---
+++ js/screens/DonorForm.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { withNavigation } from "react-navigation";
+
+class DonorForm extends Component {
+  render() {}
+}
+
+export default withNavigation(DonorForm);
