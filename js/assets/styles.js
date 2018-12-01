ab0298c420e8dd367c0a7abfd1915ea8993d44b5
elim322 <einer.lim322@gmail.com>
2018-12-01 15:58:20 -0800

    edit charity and donate tabs

--- js/assets/styles.js
+++ js/assets/styles.js
@@ -1,21 +1,21 @@
 import { Platform } from "react-native";
 
 export const colors = {
-    white: "#F9F7F7",
-    blue: "#00A0A0",
-    black: "#35322f",
-    red: "#DF5B38"
+  white: "#F9F7F7",
+  blue: "#00A0A0",
+  black: "#35322f",
+  red: "#DF5B38"
 };
 
 export const typography = {
-    baseSize: 16,
-    fontMainLight: "Montserrat-Light",
-    ...Platform.select({
-        ios: {
-            fontMain: "Montserrat"
-        },
-        android: {
-            fontMain: "Montserrat-Regular"
-        }
-    })
+  // baseSize: 16,
+  // fontMainLight: "Montserrat-Light",
+  // ...Platform.select({
+  //     ios: {
+  //         fontMain: "Montserrat"
+  //     },
+  //     android: {
+  //         fontMain: "Montserrat-Regular"
+  //     }
+  // })
 };
