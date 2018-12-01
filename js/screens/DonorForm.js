f04c24206e270301af4e9ecf43dbe30b00c3b285
Jenny rose Gutierrez <Phichik@dhcp-cc-c3-ea-10-93-15.ubcvisitor.wifi.ubc.ca>
2018-12-01 13:29:40 -0800

    creating form for donor

--- js/screens/DonorForm.js
+++ js/screens/DonorForm.js
@@ -4,9 +4,34 @@ import { Form, Field, FormSpy } from "react-final-form";
 import { Text, View, Image, ScrollView, Button } from "native-base";
 
 class DonorForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      company: "",
+      email: "",
+      address: "",
+      expDate: "",
+      food: ""
+    };
+  }
+
   render() {
     <View>
-      <Form />
+      <Form>
+        <form>
+          <fieldset>
+            <Field
+              name="company"
+              render={({ input }) => (
+                <input
+                  type="file"
+                  onChange={e => this.setState({ company: e.target.value })}
+                />
+              )}
+            />
+          </fieldset>
+        </form>
+      </Form>
     </View>;
   }
 }
